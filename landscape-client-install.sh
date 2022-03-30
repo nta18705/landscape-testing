533d30dc3f959731aa50106950a182f5ddfaac7c
Neil Anderson <53783709+nta18705@users.noreply.github.com>
2022-03-30 10:31:49 +0100

    Initial clinet install script and self-signed server cert (#1)

---
+++ landscape-client-install.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+## landscape_setup.sh
+## Neil Anderson - neil@bastion-networks.com
+##
+## Configures the client for connection to an on-prem landscape server
+
+if [ $# != "2" ]; then
+    echo "Usage: $0 [server]"
+fi
+
+$server_name = $1
+
+sudo apt-get update
+sudo apt-get install -y landscape-client
+sudo cp landscape_server_ca.crt /etc/landscape/server.pem
+sudo landscape-config --computer-title=$HOSTNAME --account-name=standalone --url=https://$server_name/message-system --ping-url=http://$server_name/ping --ssl-public-key=/etc/landscape/server.pem --silent
