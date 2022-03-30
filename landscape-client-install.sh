3207c42ab8f9c3a05dbf5b5adb5884dde17358c5
Neil Anderson <53783709+nta18705@users.noreply.github.com>
2022-03-30 12:47:05 +0100

    Added resolved restart and some feedback echoes (#4)

--- landscape-client-install.sh
+++ landscape-client-install.sh
@@ -10,9 +10,16 @@ if [ $# != "1" ]; then
     exit 0
 fi
 
+# For some reason a lot of the servers on my network need resloved restarted :/
+echo "[*] Restarting resolved"
+sudo systemctl restart systemd-resolved
+
 server_name=$1
 
-sudo apt-get update
-sudo apt-get install -y landscape-client
+echo "[*] Updating apt and installing the landscape client."
+sudo apt-get update && sudo apt-get install -y landscape-client
+echo "[*] Copying the CA certificate to /etc/landscape/server.pem"
 sudo cp landscape_server_ca.crt /etc/landscape/server.pem
+echo "[*] Configuring the landscape client"
 sudo landscape-config --computer-title=$HOSTNAME --account-name=standalone --url=https://$server_name/message-system --ping-url=http://$server_name/ping --ssl-public-key=/etc/landscape/server.pem --silent
+echo "[*] Done!"
